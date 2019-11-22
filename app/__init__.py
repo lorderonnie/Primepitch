6017b52bc7f0c8c1e5376da84af7e1442dd3c034
lorderonnie <rontheking45@gmail.com>
2019-11-22 17:34:52 +0300

    added flask script and updated my manager.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from ..config import DevConfig
 
 
 
+def create_app(config_name):
 
+    app = Flask(__name__)
 
-
-app = Flask(__name__)
-
+    app.config.from_object(DevConfig)
+    
+ 
+ 
+    return app
