110dd3ed4bb1109df645a587fc798bf0bd06062c
lorderonnie <rontheking45@gmail.com>
2019-11-25 11:03:47 +0300

    added a background image

--- config.py
+++ config.py
@@ -6,6 +6,11 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ronald:1645@localhost/primepitch' 
     SECRET_KEY = 'lorderonnie'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME='rontheking45@gmail.com'
+    MAIL_PASSWORD='ronald1645'
 
 class ProdConfig(Config):
     '''
